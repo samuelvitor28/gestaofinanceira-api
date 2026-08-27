ade553b74553180f2a4e5592fd9e1086207f61e7
Samuel Vitor Silveira da Silva Silva <samuelvitor2802@gmail.com>
2026-08-26 23:56:01 -0300

    commit inicial

---
+++ src/main/java/com/samuel/gestaofinanceira_api/GestaofinanceiraApiApplication.java
@@ -0,0 +1,13 @@
+package com.samuel.gestaofinanceira_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaofinanceiraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaofinanceiraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuel/gestaofinanceira_api/GestaofinanceiraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuel.gestaofinanceira_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaofinanceiraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
