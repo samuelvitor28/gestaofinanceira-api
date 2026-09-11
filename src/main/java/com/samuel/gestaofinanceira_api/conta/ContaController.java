90e6b641f7cb0d368e85e6dbfb8a0b12f4c93f6a
Samuel Vitor Silveira da Silva Silva <samuelvitor2802@gmail.com>
2026-09-11 19:31:24 -0300

    feat: iniciando service e controller da conta

--- src/main/java/com/samuel/gestaofinanceira_api/conta/ContaController.java
+++ src/main/java/com/samuel/gestaofinanceira_api/conta/ContaController.java
@@ -1,4 +1,28 @@
 package com.samuel.gestaofinanceira_api.conta;
 
+import com.samuel.gestaofinanceira_api.conta.dto.ContaRequestDTO;
+import com.samuel.gestaofinanceira_api.conta.dto.ContaResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contas")
 public class ContaController {
+    private final ContaService contaService;
+
+    public ContaController(ContaService contaService) {
+        this.contaService = contaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ContaResponseDTO> criarConta(@RequestBody ContaRequestDTO contaDTO){
+        return ResponseEntity.ok(contaService.criarConta(contaDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ContaResponseDTO>> listarTodos() {
+        return ResponseEntity.ok(contaService.listarTodasContas());
+    }
 }
--- src/main/java/com/samuel/gestaofinanceira_api/conta/ContaService.java
+++ src/main/java/com/samuel/gestaofinanceira_api/conta/ContaService.java
@@ -1,4 +1,38 @@
 package com.samuel.gestaofinanceira_api.conta;
 
+import com.samuel.gestaofinanceira_api.conta.dto.ContaRequestDTO;
+import com.samuel.gestaofinanceira_api.conta.dto.ContaResponseDTO;
+import com.samuel.gestaofinanceira_api.usuario.UsuarioRepository;
+import com.samuel.gestaofinanceira_api.usuario.exception.UsuarioNaoEncontradoException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ContaService {
+
+    private final ContaRepository contaRepository;
+    private final UsuarioRepository usuarioRepository;
+
+    public ContaService(ContaRepository contaRepository, UsuarioRepository usuarioRepository) {
+        this.contaRepository = contaRepository;
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public ContaResponseDTO criarConta(ContaRequestDTO dtoConta){
+        Conta novaConta = new Conta();
+        novaConta.setNome(dtoConta.nome());
+        novaConta.setUsuario(usuarioRepository.findById(dtoConta.usuarioId()).orElseThrow(()-> new UsuarioNaoEncontradoException("Usuário não encontrado: " + dtoConta.usuarioId())));
+        Conta contaSalva = contaRepository.save(novaConta);
+        return toResponseDTO(contaSalva);
+    }
+
+    public List<ContaResponseDTO> listarTodasContas(){
+        return contaRepository.findAll().stream().map(this::toResponseDTO).toList();
+    }
+
+
+    private ContaResponseDTO toResponseDTO(Conta conta){
+        return new ContaResponseDTO(conta.getId(), conta.getNome(), conta.getUsuario().getId());
+    }
 }
