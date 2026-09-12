bbae8fabd274ce59652de5c7c810d2cd4b7ea818
Samuel Vitor Silveira da Silva Silva <samuelvitor2802@gmail.com>
2026-09-12 17:04:20 -0300

    feat: começando a implementar o metodo para atualizar o nome da conta na ContaService.

--- src/main/java/com/samuel/gestaofinanceira_api/conta/ContaService.java
+++ src/main/java/com/samuel/gestaofinanceira_api/conta/ContaService.java
@@ -2,11 +2,13 @@ package com.samuel.gestaofinanceira_api.conta;
 
 import com.samuel.gestaofinanceira_api.conta.dto.ContaRequestDTO;
 import com.samuel.gestaofinanceira_api.conta.dto.ContaResponseDTO;
+import com.samuel.gestaofinanceira_api.conta.dto.ContaUpdateDTO;
 import com.samuel.gestaofinanceira_api.usuario.UsuarioRepository;
 import com.samuel.gestaofinanceira_api.usuario.exception.UsuarioNaoEncontradoException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ContaService {
@@ -31,6 +33,22 @@ public class ContaService {
         return contaRepository.findAll().stream().map(this::toResponseDTO).toList();
     }
 
+    public ContaResponseDTO alterarNomeConta(UUID idConta, ContaUpdateDTO dtoConta){
+        Conta contaParaAtualizar = contaRepository.findById(idConta).orElseThrow();
+
+        if(dtoConta.nome() == null){
+            System.out.println("Nome vazio, não pode ser substituído!");
+            throw new RuntimeException();
+        }
+
+        if(dtoConta.nome() == contaParaAtualizar.getNome()){
+            return toResponseDTO(contaParaAtualizar);
+        }
+
+        contaParaAtualizar.setNome(dtoConta.nome());
+        return toResponseDTO(contaParaAtualizar);
+
+    }
 
     private ContaResponseDTO toResponseDTO(Conta conta){
         return new ContaResponseDTO(conta.getId(), conta.getNome(), conta.getUsuario().getId());
